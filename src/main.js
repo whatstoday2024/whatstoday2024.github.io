af3011a89933ab5aa24f6f2849a2e9ecda0f1593
nina <ninaw2306@gmail.com>
2024-02-06 16:10:49 +0800

    Feat: 帳號相關頁面切版、加入 vee-validate 環境
    1. 畫面切版(註冊/登入/會員/帳戶救援頁面)、各頁面間連結設定
    2. router 加上會員頁面、帳戶救援頁面
    3. Layout 格式修改，讓內容較少的頁面高度維持 100vh
    4. 建立表單驗證 vee-validate 環境、表單內容加上初步驗證

--- src/main.js
+++ src/main.js
@@ -6,9 +6,27 @@ import router from './router'
 
 import '@/scss/all.scss'
 
+import * as VeeValidate from 'vee-validate'
+import * as VeeValidateRules from '@vee-validate/rules'
+import * as VeeValidateI18n from '@vee-validate/i18n'
+import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json'
+
+Object.keys(VeeValidateRules).forEach(rule => {
+  VeeValidate.defineRule(rule, VeeValidateRules[rule]);
+});
+VeeValidate.configure({
+  generateMessage: VeeValidateI18n.localize({ zh_TW: zhTW }),
+  validateOnInput: true
+})
+VeeValidateI18n.setLocale('zh_TW')
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.component('VForm', VeeValidate.Form)
+app.component('VField', VeeValidate.Field)
+app.component('ErrorMessage', VeeValidate.ErrorMessage)
 
 app.mount('#app')
--- src/views/RegisterView/index.vue
+++ src/views/RegisterView/index.vue
@@ -1,10 +1,72 @@
 <template>
-  <div> Register page </div>
+  <div class="container flex-fill py-4 py-lg-5 d-flex align-items-center">
+    <div class="w-100">
+      <div class="row g-3 g-lg-4 justify-content-evenly">
+        <div class="col-lg-5 align-self-lg-center">
+          <div class="text-center text-lg-end opacity-75">
+            <h1 class="display-6">
+              加入會員
+              <span class="d-block d-lg-none d-xl-block mt-3">帶您探索菜色的無限可能</span>
+              <span class="d-none d-lg-block d-xl-none mt-3">帶您探索菜色的</span>
+              <span class="d-none d-lg-block d-xl-none mt-3">無限可能</span>
+            </h1>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <VForm class="card rounded-4 p-4 p-lg-5" v-slot="{ errors }" @submit="register">
+            <h3 class="h2 mb-3 text-center">註冊帳號</h3>
+            <div class="text-center">
+              <button type="button" class="btn btn-outline-brand-blue mb-4 rounded-pill" @click="$router.push({name: 'Login'})">已有會員，前往登入</button>
+            </div>
+            <div class="form-floating mb-4">
+              <VField type="text" id="username" placeholder="請輸入姓名" name="姓名" class="form-control" :class="{ 'is-invalid': errors['姓名'] }" rules="required" v-model="user.name" />
+              <ErrorMessage name="姓名" class="invalid-feedback"/>
+              <label for="username">請輸入姓名</label>
+            </div>
+            <div class="form-floating mb-4">
+              <VField type="email" id="email" placeholder="請輸入電子信箱" name="信箱" class="form-control" :class="{ 'is-invalid': errors['信箱'] }" rules="email|required" v-model="user.email" />
+              <ErrorMessage name="信箱" class="invalid-feedback"/>
+              <label for="email">請輸入電子信箱</label>
+            </div>
+            <div class="form-floating mb-4">
+              <VField type="password" id="password" placeholder="請輸入密碼" name="密碼" class="form-control" :class="{ 'is-invalid': errors['密碼'] }" rules="required" v-model="user.password" />
+              <ErrorMessage name="密碼" class="invalid-feedback"/>
+              <label for="password">請輸入密碼</label>
+            </div>
+            <div class="form-floating mb-4">
+              <VField type="password" id="passwordDoubleCheck" placeholder="再次確認密碼" name="再次確認密碼" class="form-control" :class="{ 'is-invalid': errors['再次確認密碼'] }" :rules="checkPassword" v-model="passwordDoubleCheck" />
+              <ErrorMessage name="再次確認密碼" class="invalid-feedback"/>
+              <label for="passwordDoubleCheck">再次確認密碼</label>
+            </div>
+            <button type="submit" class="btn btn-brand-blue btn-lg rounded-pill">註冊</button>
+          </VForm>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      user: {
+        name: '',
+        email: '',
+        password: '',
+      },
+      passwordDoubleCheck: ''
+    }
+  },
+  methods: {
+    register(){
+      // API 註冊會員
+    },
+    checkPassword(value) {
+      if (!value) return '確認密碼 為必填'
+      else return value === this.user.password ? true : '兩次輸入的密碼不一致'
+    }
+  }
 }
 </script>
 
